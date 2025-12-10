d897f772ec990b1fcf8d3baa85a68ca9955feb40
Gabriel <gabrielbr190@gmail.com>
2025-12-10 14:04:19 +0100

    Primeros cambios realizados

--- registrar.php
+++ registrar.php
@@ -10,22 +10,55 @@
 <body>
     <?php
     // Crear carpetas si no existen
+    $carpeta = "data";
+    $subcarpeta = "./data/logs";
     // TODO
+    if(!is_dir($carpeta) && !is_dir($subcarpeta)){
+        mkdir($carpeta, 0755);
+        mkdir($subcarpeta, 0755);
+        echo "Carpetas creadas correctamente.<br>";
+    } 
 
-    if ($_POST) {
+    if (isset($_POST['accion']) && $_POST['accion'] == 'registrar') {
         // TODO
+        $nombre = trim($_POST['nombre']);
+        $curso = trim($_POST['curso']);
+        $edad = intval(trim($_POST['edad']));
+        $menu = trim($_POST['menu'] ?? '');
+
+        if($nombre == "" || $curso == "" || $edad == "" || $menu == ""){
+            echo "Por favor, completa todos los campos.<br>";
+        }else{
+            $elfo = [
+                "nombre" => $nombre,
+                "edad" => $edad,
+                "curso" => $curso,
+                "menu" => $menu
+            ];
+            chdir($carpeta);
+            $json_string = json_encode($elfo,JSON_PRETTY_PRINT);
+            $archivo = 'elfos.json';
+            file_put_contents($archivo, $json_string);
+            echo "Datos guardados correctamentes en $archivo";
+
+            $subcarpeta = "logs";
+            chdir($subcarpeta);
+            $datos = date("Y-m-d H:i:s") . "- Registrado: " . $nombre . "\n";
+            $texto = "registro.log";
+            file_put_contents($texto,$datos);
+        }
     } ?>
-    <form method="POST">
-        Nombre y apellidos: <input type="text" name="nombre" required><br>
-        Curso: <input type="text" name="curso" required><br>
-        Edad: <input type="number" name="edad" required><br>
+    <form action="registrar.php" method="POST">
+        Nombre y apellidos: <input type="text" name="nombre"  id="nombre" required><br>
+        Curso: <input type="text" name="curso" id="curso" required><br>
+        Edad: <input type="number" name="edad"  id="edad" required><br>
         <select name="menu" id="alergia">
             <option value="" disabled selected>--Seleccione un menú--</option>
             <option value="Estandar">Estándar</option>
             <option value="Lactosa">Sin lactosa</option>
             <option value="Gluten">Sin gluten</option>
         </select>
-        <button type="submit">Registrar elfo</button>
+        <button type="submit" name="accion" value="registrar">Registrar elfo</button>
     </form>
     <br>
     <a href="index.html">Volver al inicio</a>
