f9334b78ad6d41bd3c49c10ad74a76a61df1a852
Alejandro <alexrabosomartos@gmail.com>
2025-12-17 13:57:45 +0100

    Buscar_un_elfo

--- buscar.php
+++ buscar.php
@@ -1,18 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <form method="GET">
-        Buscar elfo por nombre: <input type="text" name="busca">
-        <button type="submit">Buscar</button>
+    <form action="buscar.php" method="post">
+        Buscar elfo por nombre: <input type="text" name="nombre">
+        <button type="submit" name="accion" value="busca">Buscar</button>
     </form>
     <br><a href="index.html">Volver al inicio</a><br><br>
     <?php
-    // TODO
+    // Selecciono la carpeta en la que voy a buscar
+    $carpeta = "data";
+    $archivo = 'data/elfos.json';
+
+    if (isset($_POST['accion']) && $_POST['accion'] == 'busca') {
+        $nombre = strtolower(trim($_POST['nombre']));
+
+        if ($nombre == "") {
+            echo "Por favor, completa todos los campos.<br>";
+        } else {
+            // Leer datos existentes si el archivo existe
+            $array_elfos = [];
+            if (file_exists($archivo)) {
+                $json_existing = file_get_contents($archivo);
+                $array_elfos = json_decode($json_existing, true);
+            }
+
+            $infoElfo;
+            $condicion = false;
+            foreach ($array_elfos as $elfo) {
+                if (str_contains(strtolower(trim($elfo['nombre'])), $nombre)) {
+                    $condicion = true;
+                    $infoElfo = $elfo;
+                }
+            }
+
+            if ($condicion == true) {
+                echo "Información del elfo: <br>";
+                echo "Nombre: " . $infoElfo['nombre'] . "<br>";
+                echo "Edad: " . $infoElfo['edad'] . "<br>";
+                echo "Curso: " . $infoElfo['curso'] . "<br>";
+                echo "Menu: " . $infoElfo['menu'] . "<br>";
+                /* $numero = 0;
+                foreach ($infoElfo as $info) {
+                    echo $infoElfo[$numero] . " : ";
+                    echo $info . "<br>";
+
+                    $numero++;
+                }*/
+            } else {
+                echo "No existe ese elfo";
+            }
+        }
+    }
     ?>
 </body>
+
 </html>
